41e41e6496b21ccd0931424c6816fed0b2d053f4
Donnie-Ice <dri0001@mix.wvu.edu>
2025-07-02 10:08:45 -0400

    [nasa/nos3#619] Fixed CFDP

--- fsw/tables/cf_def_config.c
+++ fsw/tables/cf_def_config.c
@@ -26,19 +26,19 @@
 #include "cf_tbldefs.h"
 
 CF_ConfigTable_t CF_config_table = {
-    1,    /* ticks_per_second */
+    10,    /* ticks_per_second */
     16384, /* max number of bytes per wakeup to calculate r2 recv file crc */
     25,    /* local entity id */
     {      /* channel configuration for CF_NUM_CHANNELS */
      {
          /* channel 0 */
-         2,      /* max number of outgoing messages per wakeup */
-         2,      /* max number of rx messages per wakeup */
+         5,      /* max number of outgoing messages per wakeup */
+         5,      /* max number of rx messages per wakeup */
          3,      /* ack timer */
          3,      /* nak timer */
          30,     /* inactivity timer */
-         4,      /* ack limit */
-         4,      /* nak limit */
+         5,      /* ack limit */
+         5,      /* nak limit */
          0x18c8, /* input message id */
          0x08c2, /* output message id */
          16,     /* input pipe depth */
